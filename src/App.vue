388eca7ea3bed8160732604737508f2687266fbb
Kylie <kylie8399@gmail.com>
2021-09-21 13:37:26 -0400

    started the header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <ul class="header">
+        <li class="imgfix">
+          <img src="./assets/logo.png" alt="" srcset="" />
+        </li>
+        <li>
+          <router-link to="/"> HOME </router-link>
+        </li>
+        <li><router-link to="/about"> ABOUT </router-link></li>
+        <li><router-link to="/contact"> CONTACT ME </router-link></li>
+        <li><router-link to="/bookonline"> BOOK ONLINE </router-link></li>
+
+        <li class="mobile-button" @click="showMobileMenu">
+          <font-awesome-icon icon="bars" />
+        </li>
+      </ul>
+    </div>
+  </div>
+     <!-- Mobile menu -->
+      <div class="mobile-menu" v-if="mobileVisible">
+        <ul @click="showMobileMenu">
+          <li>
+            <router-link to="/"> HOME </router-link>
+          </li>
+          <li><router-link to="/about"> ABOUT </router-link></li>
+          <li><router-link to="/contact"> CONTACT US </router-link></li>
+          <li><router-link to="/bookonline"> BOOK ONLINE</router-link></li>
+        </ul>
+      </div>
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      mobileVisible: false,
+    };
+  },
+  methods: {
+    showMobileMenu() {
+      this.mobileVisible = !this.mobileVisible;
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +63,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.header {
+  list-style: none;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: left;
+  padding-inline-start: 0;
+  vertical-align: middle;
+  min-height: 80px;
+  border-bottom: 1px solid rgba(189, 23, 22, 0.6);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from '@/components/Home'
+import About from '@/components/About'
+import Bookonline from '@/components/Bookonline'
+import Contact from '@/components/Contact'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Optionally install the BootstrapVue icon components plugin
 
 Vue.config.productionTip = false
-
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About},
+  { path: '/contact', component: Contact },
+  { path: '/bookonline', component: Bookonline },
+]
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 new Vue({
   render: function (h) { return h(App) },
 }).$mount('#app')
