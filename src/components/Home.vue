8a57d0a3df662eafdc435f51ee25560e346c17d3
Kylie <kylie8399@gmail.com>
2021-09-23 14:23:34 -0400

    changes to homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,31 @@
-<template></template>
+<template>
+  <div class="hero">
+    <h2><span>Fitness</span> <span>Health</span> <span>Happiness</span></h2>
+    <img src="../assets/fitpic4.jpg" />
+  </div>
+</template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
+export default {};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hero img {
+  min-width: 100%;
+  height: 100vh;
+}
+.hero2-top .border {
+  width: 100%;
+  max-width: 160px;
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  border-bottom-color: red;
+  border-bottom-width: 5px;
+  border-bottom-style: solid;
+}
+.hero2-top {
+  display: flex;
+  justify-content: center;
+  padding-top: 100px;
+}
 </style>
