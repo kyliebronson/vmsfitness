f294d3164e32d737a672c7ca14dc2e8a7fb8963b
Kylie <kylie8399@gmail.com>
2021-10-06 12:09:21 -0400

    added book online page

--- src/components/Bookonline.vue
+++ src/components/Bookonline.vue
@@ -1,14 +1,40 @@
-<template></template>
+<template>
+  <iframe
+    src="https://calendly.com/kyliebfit/15minfirstconsult"
+    frameborder="0"
+    width="100%"
+    height="800px"
+  ></iframe>
+</template>
 
-<script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+p {
+  text-transform: uppercase;
+  padding-top: 30px;
+  font-size: 45px;
+}
+p {
+  padding: 30px;
+}
+
+.main {
+  position: relative;
+  width: 100%;
+  top: 20px;
+}
+.image {
+  position: absolute;
+  left: 20%;
+  border: 2px solid;
+  width: 28%;
+}
+.paragraph {
+  position: absolute;
+  left: 43%;
+  border: 2px solid;
+  background-color: white;
+  padding: 18px;
+  width: 400px;
+  top: 65px;
+}
 </style>
